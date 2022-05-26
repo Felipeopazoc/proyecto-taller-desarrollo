4c1557f6d5cee64d702a539f4502681dfaf77009
felipe.opazo1901 <felipe.opazo1901@alumnos.ubiobio.cl>
2022-05-25 22:31:47 -0400

    set hora de chile y charset utf 8

--- conexion_bd/conexion.php
+++ conexion_bd/conexion.php
@@ -9,6 +9,7 @@ $username= "root";
 $password = "";
 date_default_timezone_set('America/Santiago');
 $conn = new mysqli($host,$username,$password,$dbname);
+$conn->set_charset("utf8");
 
 }catch(Exception $e){
 echo "Error: ".$e->getMessage();
